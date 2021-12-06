5820b0fdb60fb66a8f0fc49efd529ae465c02616
Cheng <cyu5@pm.me>
2021-12-05 18:25:43 -0600

    remove rubocop offenses

--- loan_calculator/loan_calculator.rb
+++ loan_calculator/loan_calculator.rb
@@ -5,38 +5,38 @@ init_message_constants()
 # main program loop
 loop do
 
-	# start of interactive mode
-	system('clear') or system('cls')
-	puts WELCOME
+  # start of interactive mode
+  system('clear') or system('cls')
+  puts WELCOME
 
-	# asks for user inputs
-	loan_amount = 
-		insists ENTER_AMOUNT, :to_loan_amount, INVALID_AMOUNT
-	term_in_months = 
-		insists ENTER_MONTHS, :to_term_in_months, INVALID_MONTHS
-	apr = 
-		insists ENTER_APR, :to_apr, INVALID_APR
+  # asks for user inputs
+  loan_amount =
+    insists ENTER_AMOUNT, :to_loan_amount, INVALID_AMOUNT
+  term_in_months =
+    insists ENTER_MONTHS, :to_term_in_months, INVALID_MONTHS
+  apr =
+    insists ENTER_APR, :to_apr, INVALID_APR
 
-	# calculate results
-	monthly_rate = 
-		apr.fdiv(12)
-	monthly_payment = 
-		calculate_monthly_payment(loan_amount, monthly_rate, term_in_months)
-	total_payments = 
-		monthly_payment * term_in_months
-	total_interest = 
-		total_payments - loan_amount
+  # calculate results
+  monthly_rate =
+    apr.fdiv(12)
+  monthly_payment =
+    calculate_monthly_payment(loan_amount, monthly_rate, term_in_months)
+  total_payments =
+    monthly_payment * term_in_months
+  total_interest =
+    total_payments - loan_amount
 
-	# print results
-	puts RESULTS
-	puts "#{MONTHLY_PAYMENT.ljust(25)}#{dollar_format(monthly_payment)}"  # need formating to 2 decimal and a leading $ sign
-	puts "#{MONTHLY_RATE.ljust(25)}#{percentage_format(monthly_rate)}" # need formating to 3 significant digits and trailing % sign
-	puts "#{NUM_PAYMENTS.ljust(25)}#{term_in_months}"
-	puts "#{TOTAL_PAYMENTS.ljust(25)}#{dollar_format(total_payments)}"  # need formating to 2 decimal and a leading $ sign
-	puts "#{TOTAL_INTEREST.ljust(25)}#{dollar_format(total_interest)}"  # need formating to 2 decimal and a leading $ sign
+  # print results
+  puts RESULTS
+  puts "#{MONTHLY_PAYMENT.ljust(25)}#{dollar_format(monthly_payment)}"
+  puts "#{MONTHLY_RATE.ljust(25)}#{percentage_format(monthly_rate)}"
+  puts "#{NUM_PAYMENTS.ljust(25)}#{term_in_months}"
+  puts "#{TOTAL_PAYMENTS.ljust(25)}#{dollar_format(total_payments)}"
+  puts "#{TOTAL_INTEREST.ljust(25)}#{dollar_format(total_interest)}"
 
-	# exit program if user doesn't input y or yes
-	break puts(GOODBYE) unless start_over?()
+  # exit program if user doesn't input y or yes
+  break puts(GOODBYE) unless start_over?()
 end
 
 # TODOS:
--- loan_calculator/loan_calculator_helper.rb
+++ loan_calculator/loan_calculator_helper.rb
@@ -1,14 +1,13 @@
 require "yaml"
 
-def init_message_constants()
+def init_message_constants
+  # extract program messages as constants at runtime
+  YAML.load_file("loan_calculator_msg.yml").each_pair do |key, value|
+    Object.const_set(key.to_s.upcase, value)
+  end
 
-	# extract program messages as constants at runtime
-	YAML.load_file("loan_calculator_msg.yml").each_pair do |key, value|
-		Object.const_set(key.to_s.upcase, value)
-	end
-
-	# explicit return nil
-	nil
+  # explicit return nil
+  nil
 end
 
 # @param {Float} amount
@@ -16,66 +15,66 @@ end
 # @param {Integer} months
 # @return {Float}
 def calculate_monthly_payment(amount, percent_rate, months)
-	rate = percent_rate.fdiv(100)
-	payment = amount * (rate / (1 - (1 + rate)**(-months)))
+  rate = percent_rate.fdiv(100)
+  amount * (rate / (1 - (1 + rate)**(-months)))
 end
 
 # @param {String} prompt
 # @return {String}
 def asks(prompt="")
-	print prompt
-	gets.chomp
+  print prompt
+  gets.chomp
 end
 
 # @param {String} prompt
 # @param {Symbol} processor
 # @param {String} retry_msg
-# @return {not nil} 
+# @return {not nil}
 def insists(prompt, parser, retry_msg)
-	loop do
-		raw_input = asks prompt
-		result = send(parser, raw_input)
-		break result unless result == nil
-		puts retry_msg
-	end
+  loop do
+    raw_input = asks prompt
+    result = send(parser, raw_input)
+    break result unless result.nil?
+    puts retry_msg
+  end
 end
 
 # @param {String} input_str
 # @return {Float, nil}
 def to_loan_amount(input_str)
-	float = input_str.to_f
-	float.truncate(2)
-	float.positive? ? float : nil
+  float = input_str.to_f
+  float.truncate(2)
+  float.positive? ? float : nil
 end
 
 # @param {String} input_str
 # @return {Integer, nil}
 def to_term_in_months(input_str)
-	int = input_str.to_i
-	int.positive? ? int : nil
+  int = input_str.to_i
+  int.positive? ? int : nil
 end
 
 # @param {String} input_str
 # @return {Float, nil}
 def to_apr(input_str)
-	percent = input_str.to_f
-	percent.positive? ? percent : nil
+  percent = input_str.to_f
+  percent.positive? ? percent : nil
 end
 
 # @param {Float} float
 # @return {String}
 def dollar_format(float)
-	"$#{format('%.2f', float)}"
+  "$#{format('%.2f', float)}"
 end
 
 # @param {Float} percent
 # @return {String}
 def percentage_format(percent)
-	"#{format('%.2g', percent)}%"
+  "#{format('%.2g', percent)}%"
 end
 
 # @return {Boolean}
-def start_over?()
-	answer = asks(START_OVER).downcase
-	answer == 'y' or answer == 'yes'
+def start_over?
+  answer = asks(START_OVER).downcase
+  answer == 'y' or answer == 'yes'
 end
